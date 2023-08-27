721d84b5a0c093d43e73a2a62df884937248a66a
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-08-27 18:08:09 +0530

    added more feature with enhancement

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
 
 function capitalizeText(text: string): string {
+  if (text.length === 0) {
+    return text;
+  }
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
@@ -13,13 +16,19 @@ function toUpperCase(text: string): string {
 }
 
 function toCamelCase(text: string): string {
-  const words = text.split(' ');
+  const words = text.split(/\s+/);
   const camelCaseWords = words.map((word, index) => (index === 0 ? word.toLowerCase() : capitalizeText(word)));
   return camelCaseWords.join('');
 }
 
 function toSnakeCase(text: string): string {
   return text.replace(/\s+/g, '_').toLowerCase();
+}
+
+function toPascalCase(text: string): string {
+  const words = text.split(/\s+/);
+  const pascalCaseWords = words.map((word) => capitalizeText(word));
+  return pascalCaseWords.join('');
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -29,7 +38,9 @@ export function activate(context: vscode.ExtensionContext) {
     { name: 'camelcase', method: toCamelCase },
     { name: 'snakecase', method: toSnakeCase },
     { name: 'capitalize', method: capitalizeText },
+    { name: 'pascalcase', method: toPascalCase },
   ];
+  
   functionalities?.forEach(functionality => {
     let disposable = vscode.commands.registerCommand(`text-case-changer.${functionality?.name}`, () => {
       const editor = vscode.window.activeTextEditor;
